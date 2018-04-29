cfa9b776f0bb2ba5ab5cb65874a4711394e822e9
Paulo Chang <paulo.changr@gmail.com>
2018-04-29 11:47:16 +0200

    Moved libraries back to original destination. Move methods to public location. Add new subdirectory to building location.

--- assignment1/src/Leg/Btime/btime.cpp
+++ assignment1/src/Leg/Btime/btime.cpp
@@ -9,17 +9,20 @@ int DayCountCalculator::getNDaysYear() {
 }
 
 //Actual_360 - compute_daycount_boost(bfrom, bto)
-double Actual_360::compute_daycount_boost(const boost::gregorian::date& bfrom, const boost::gregorian::date& bto) {
+double
+Actual_360::compute_daycount_boost(const boost::gregorian::date &bfrom, const boost::gregorian::date &bto) const {
 	return (bto - bfrom).days();
 }
 
 //Thirty_360 - compute_daycount_1(years, months, days_from, days_to)
-double Thirty_360::compute_daycount_1(const short years, const short months, const short days_from, const short days_to) {
+double Thirty_360::compute_daycount_1(const short years, const short months, const short days_from,
+									  const short days_to) const {
 	return ((360 * years) + (30 * (months - 1)) + std::max<short>(0, (30 - days_from)) + std::min<short>(30, days_to));
 }
 
 //Thirty_360 - compute_daycount_boost(bfrom, bto)
-double Thirty_360::compute_daycount_boost(const boost::gregorian::date& bfrom, const boost::gregorian::date& bto) {
+double
+Thirty_360::compute_daycount_boost(const boost::gregorian::date &bfrom, const boost::gregorian::date &bto) const {
 	auto from_date{bfrom.year_month_day()};
 	auto to_date{bto.year_month_day()};
 	auto years = to_date.year - from_date.year;
@@ -28,7 +31,7 @@ double Thirty_360::compute_daycount_boost(const boost::gregorian::date& bfrom, c
 }
 
 //DayCountCalculator - compute_daycount(string_from, string_to)
-double DayCountCalculator::compute_daycount(const std::string& string_from, const std::string& string_to) {
+double DayCountCalculator::compute_daycount(const std::string &string_from, const std::string &string_to) const {
 	boost::gregorian::date from_date{boost::gregorian::from_string(string_from)};
 	boost::gregorian::date to_date{boost::gregorian::from_string(string_to)};
 	return compute_daycount_boost(from_date, to_date);
