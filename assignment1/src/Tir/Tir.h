24a6811ed6d1c672a5865c02a7ae836e5565b0b7
Eddy Castro <eddyomar.castro@alumnos.uc3m.es>
2018-05-01 17:42:06 +0200

    Newton Raphson using boost implemented. (test fail - incorrect result)

--- assignment1/src/Tir/Tir.h
+++ assignment1/src/Tir/Tir.h
@@ -51,15 +51,80 @@ class Bono_TIR {
 //namespace tools {
 
 class Bono_TIR{
+	
+	
+public:
+	
+	/*
+	static double notional;
+	static std::vector<double> &aCashFlows;
+	static int sPeriods;
+	static double tPPeriods;
+	static int mPeriod;
+	*/
+	
+  //static double notional;// = 100;
+	//static std::vector<double> CashFlows;// = {2.99, 2.98, 2.97, 101.60};
+	//static int sPeriods;// = 2;
+  //static double tPPeriods;// = 2.0;
+	//static int mPeriod;// = 1;
 
+	
+	/*
+  double operator()(const double notional_p, const std::vector<double> &aCashFlows_p, const int sPeriods_p, const double tPPeriods_p, const int mPeriod_p){
+		notional = notional_p;
+		aCashFlows = aCashFlows_p;
+		sPeriods = sPeriods_p;
+		tPPeriods = tPPeriods_p;
+		mPeriod = mPeriod_p;
+  };
+	*/
+	
+	/*
+	Bono_TIR(const double notional_p, const std::vector<double> &aCashFlows_p, const int sPeriods_p, const double tPPeriods_p, const int mPeriod_p){
+		notional = notional_p;
+		aCashFlows = aCashFlows_p;
+		sPeriods = sPeriods_p;
+		tPPeriods = tPPeriods_p;
+		mPeriod = mPeriod_p;
+	};
+	*/
+	
+	
+	static double value (double tSubperiods, double pIncrement, double actIRate, std::vector<double> CashFlows){
+		int i = 0, j;
+		
+		double bValue = 0.0;
+		double notional = 100;
+		for(j = 0; j < tSubperiods; j++){
+			double actInc = ((j + 1) * pIncrement);
+			//bValue += (aCashFlows.at(j) + ((j == (tSubperiods - 1)) ? notional : 0)) * std::exp(-(actIRate * actInc));
+			bValue += (CashFlows.at(j) + ((j == (tSubperiods - 1)) ? notional : 0)) * std::exp(-(actIRate * actInc));
+		}
+		return bValue;
+	}
+	
+	static double der_value(double tSubperiods, double pIncrement, double actIRate, std::vector<double> CashFlows){
+		int i = 0, j;
+		double notional = 100;
+		double derBValue = 0.0;
+		for(j = 0; j < tSubperiods; j++){
+			double actInc = ((j + 1) * pIncrement);
+			//derBValue += (-actInc) * (aCashFlows.at(j) + ((j == (tSubperiods - 1)) ? notional : 0)) * std::exp(-(actIRate * actInc));
+			derBValue += (-actInc) * (CashFlows.at(j) + ((j == (tSubperiods - 1)) ? notional : 0)) * std::exp(-(actIRate * actInc));
+		}
+		return derBValue;
+	}
+	
 	template <class T>
 	struct cbrt_functor
 	{
 	   cbrt_functor(T const& target) : a(target){}
 	   std::tuple<T, T> operator()(T const& z)
 	   {
-	      T sqr = z * z;
-	      return std::make_tuple(sqr * z - a, 3 * sqr);
+	      //T sqr = z * z;
+	      //return std::make_tuple(sqr * z - a, 3 * sqr);
+			 return std::make_tuple(value(4, 0.5, z, {5.0, 5.8, 6.4, 6.8}), der_value(4, 0.5, z, {5.0, 5.8, 6.4, 6.8}));
 	   }
 	private:
 	   T a;
@@ -72,9 +137,9 @@ public:
 	   using namespace std;
 	   int exp;
 	   frexp(z, &exp);
-	   T min = ldexp(0.5, exp/3);
-	   T max = ldexp(2.0, exp/3);
-	   T guess = ldexp(1.0, exp/3);
+	   T min = 0;//ldexp(0.5, exp/3);
+	   T max = 10;//ldexp(2.0, exp/3);
+	   T guess = z;//ldexp(1.0, exp/3);
 	   int digits = std::numeric_limits<T>::digits;
 	   return boost::math::tools::newton_raphson_iterate(cbrt_functor<T>(z), guess, min, max, digits);
 	}
--- assignment1/src/Tir/tests/test.cpp
+++ assignment1/src/Tir/tests/test.cpp
@@ -15,12 +15,14 @@ BOOST_AUTO_TEST_SUITE(tir_test_suite)
 			  const double tPPeriods = 2.0;
 				const int mPeriod = 1;
 				
+				//Bono_TIR bond = Bono_TIR(notional, CashFlows, sPeriods, tPPeriods, mPeriod);
+				
 				Bono_TIR bond = Bono_TIR();
         
-				double expected_values = 	3.0;//6.76;
+				double expected_values = 	6.76;
 				//double calculated_values = bond(notional, CashFlows, sPeriods, tPPeriods, mPeriod);
         
-				double calculated_values = bond.cbrt(27.0);
+				double calculated_values = bond.cbrt(6.76);
 
         BOOST_TEST_MESSAGE(" - Calculated Value: " << calculated_values);
         BOOST_TEST_MESSAGE(" - Expected Value: " << expected_values);
